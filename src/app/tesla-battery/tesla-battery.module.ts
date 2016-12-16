78c3c25d81ee01b84f07968407b029d8d4cb3b0b
Eric Villeneuve <eric.villeneuve@paysafe.com>
2016-12-16 13:22:23 -0500

    Stats Component.  (https://toddmotto.com/building-tesla-range-calculator-angular-2-reactive-forms#stats-component)

--- src/app/tesla-battery/tesla-battery.module.ts
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -6,11 +6,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BatteryService } from './tesla-battery.service';
 // containers
 import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.component';
+// components
+import { TeslaCarComponent } from './components/tesla-car/tesla-car.component';
 
 @NgModule({
     declarations: [
         // registering our container component
-        TeslaBatteryComponent
+        TeslaBatteryComponent,
+        TeslaCarComponent
     ],
     imports: [
         CommonModule,
